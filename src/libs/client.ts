5a75c4920e84a61c6167ee062c836efe55f2eb7d
shamo-9118 <won9118391183@gmail.com>
2022-06-23 19:05:19 +0900

    ✨API_KEYを環境変数にで作成

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -3,5 +3,5 @@ import { createClient } from 'microcms-js-sdk'; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "shomablog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: "cdbf1874111f4b549c47732c03f5c9c90531",
+  apiKey: process.env.API_KEY,
 });
