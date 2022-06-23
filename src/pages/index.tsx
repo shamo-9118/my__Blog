0a0c3b0f69474414b246f68f8d778917ae35a336
shamo-9118 <won9118391183@gmail.com>
2022-06-23 09:47:32 +0900

    ⚡️ cmsからコンテンツのタイトルを取得し表示させる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from "next";
+import { client } from "../libs/client";
+import { GetStaticProps } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import Link from "next/link";
+export type Blog = {
+  title: string;
+  body: string;
+};
+type Props = MicroCMSListResponse<Blog>;
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+const Home: NextPage<Props> = (props) => {
+  return (
+    <div>
+      <p>{`記事の総件数: ${props.totalCount}件`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`/blog/${content.id}`}>
+                <a>{content.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
+  return {
+    props: data,
+  };
 };
 
 export default Home;
