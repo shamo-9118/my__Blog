785cea6d51ef85e0d1a584314051a8753f86590f
shamo-9118 <won9118391183@gmail.com>
2022-06-23 21:41:59 +0900

    ⚡️ APIrouteを使ってAPIを実装、検索時の処理をサーバー側で行うように処理をapiに記述

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { client } from "../libs/client";
 import { GetStaticProps } from "next";
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import Link from "next/link";
-import { ComponentProps, FormEventHandler } from "react";
+import { ComponentProps, FormEventHandler, useState } from "react";
 export type Blog = {
   title: string;
   body: string;
@@ -11,9 +11,26 @@ export type Blog = {
 type Props = MicroCMSListResponse<Blog>;
 
 const Home: NextPage<Props> = (props) => {
+  const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
+
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = async (e) => {
+    e.preventDefault(); //onSubmitのデフォルトの挙動をキャンセルする処理
+    const q = e.currentTarget.query.value;
+    const data = await fetch("/api/search", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ q }),
+    });
+    const json:MicroCMSListResponse<Blog> = await data.json();
+    setSearch(json);
+  };
+
+  console.log(search);
+  
+
   return (
     <div>
-      <form className="flex gap-x-2" >
+      <form className="flex gap-x-2" onSubmit={handleSubmit}>
         <input
           type="text"
           name="query"
