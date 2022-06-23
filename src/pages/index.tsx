ae1c5e9cdb76d97ba40b41c016e074998bdaa093
shamo-9118 <won9118391183@gmail.com>
2022-06-23 19:44:05 +0900

    💄 検索とボタンのスタイリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { client } from "../libs/client";
 import { GetStaticProps } from "next";
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import Link from "next/link";
+import { ComponentProps, FormEventHandler } from "react";
 export type Blog = {
   title: string;
   body: string;
@@ -12,13 +13,23 @@ type Props = MicroCMSListResponse<Blog>;
 const Home: NextPage<Props> = (props) => {
   return (
     <div>
-      <p className=" text-gray-400">{`記事の総件数: ${props.totalCount}件`}</p>
+      <form className="flex gap-x-2" >
+        <input
+          type="text"
+          name="query"
+          className="border border-gray-600 px-2"
+        />
+        <button className="border border-gray-600 px-2">検索</button>
+      </form>
+      <p className="mt-4 text-gray-400">{`記事の総件数: ${props.totalCount}件`}</p>
       <ul className="mt-4 space-y-4">
         {props.contents.map((content) => {
           return (
             <li key={content.id}>
               <Link href={`/blog/${content.id}`}>
-                <a className="underline text-blue-800 text-xl hover:text-blue-400" >{content.title}</a>
+                <a className="text-xl text-blue-800 underline hover:text-blue-400">
+                  {content.title}
+                </a>
               </Link>
             </li>
           );
@@ -29,7 +40,10 @@ const Home: NextPage<Props> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const data = await client.getList<Blog>({ endpoint: "blog" });
+  const data = await client.getList<Blog>({
+    endpoint: "blog",
+    // queries: { q: "test" },
+  });
   return {
     props: data,
   };
