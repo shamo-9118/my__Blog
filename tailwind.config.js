591beeb3650b115d6b6d3b1fabe76e5bbeef7b2e
shamo-9118 <won9118391183@gmail.com>
2022-06-27 15:29:16 +0900

    💄 tailwindにテーマを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: { 
+    extend: {fontFamily:{Arial:"Arial Rounded MT Bold"}} },
   plugins: [
     require('@tailwindcss/typography'),
   ],
