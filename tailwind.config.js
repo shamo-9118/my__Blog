ebfc173fb169677828d280ce94d082dfcd5b35a5
shamo-9118 <won9118391183@gmail.com>
2022-06-27 22:39:42 +0900

    💄 tailwindcssにfontfamiryを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { 
-    extend: {fontFamily:{Arial:"Arial Rounded MT Bold"}} },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-  
+  theme: {
+    screen: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
+    extend: {
+      fontFamily: {
+        Arial: "Arial Rounded MT Bold",
+        Avenir: "Avenir",
+        Tsukushiba: "築柴B丸ゴシック",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
