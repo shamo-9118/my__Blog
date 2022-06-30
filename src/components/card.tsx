5d351ddc65c1b1b25236befc06089d242d146980
shamo-9118 <won9118391183@gmail.com>
2022-06-30 14:12:52 +0900

    💄card componentsを作成

---
+++ src/components/card.tsx
@@ -0,0 +1,45 @@
+import { Card, Image, Text, Group } from "@mantine/core";
+import { Badge } from "@mantine/core";
+import { Bold } from "tabler-icons-react";
+
+const CardBadge = () => {
+  return <Badge color="dark" size="sm" radius="sm"></Badge>;
+};
+
+export const MantineCard = () => {
+  return (
+    <div style={{ width: 320, margin: "auto" }}>
+      <Card
+        shadow="sm"
+        p="xl"
+        component="a"
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        target="_blank"
+      >
+        <Card.Section>
+          <Image
+            src="Typescript_logo_2020.svg.jpeg"
+            height={200}
+            alt="No way!"
+          />
+        </Card.Section>
+
+        <Text weight="Bold" size="lg" className=" my-3 ">
+         今日ぼくが考えた最強になれるアイデア三選
+        </Text>
+
+        <Text size="xs" className="mb-1">
+          2022-6-30
+        </Text>
+        <Group className="gap-2">
+          <Badge color="dark" size="xs" radius="sm">
+            ・インタビュー
+          </Badge>
+          <Badge color="dark" size="xs" radius="sm">
+            ・エンジニア
+          </Badge>
+        </Group>
+      </Card>
+    </div>
+  );
+};
---
+++ src/pages/postsList.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from "next";
+import { client } from "../libs/client";
+import { GetStaticProps } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import Link from "next/link";
+import { ComponentProps, FormEventHandler, useState } from "react";
+
+export type Blog = {
+  title: string;
+  body: string;
+};
+type Props = MicroCMSListResponse<Blog>;
+
+const Blog: NextPage<Props> = (props) => {
+  const opened =()=>{
+    return 
+  }
+  const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
+
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = async (e) => {
+    e.preventDefault(); //onSubmitのデフォルトの挙動をキャンセルする処理
+    const q = e.currentTarget.query.value;
+    const data = await fetch("/api/search", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ q }),
+    });
+    const json: MicroCMSListResponse<Blog> = await data.json();
+    setSearch(json);
+  };
+
+  const contents = search ? search.contents : props.contents;
+  const totalCount = search ? search.totalCount : props.totalCount;
+  return (
+    <div>
+      <ul className="mt-4 space-y-4">
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`/blog/${content.id}`}>
+                <a className="text-xl text-blue-800 underline hover:text-blue-400">
+                  {content.title}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({
+    endpoint: "blog",
+    // queries: { q: "test" },
+  });
+  return {
+    props: data,
+  };
+};
+
+export default Blog;
