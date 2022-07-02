99d33e9187b2c07417c3515e78e01236e0555afd
shamo-9118 <won9118391183@gmail.com>
2022-07-02 13:21:39 +0900

    ♻️top menu tabのリファクタリング

--- src/components/topMenu.tsx
+++ src/components/topMenu.tsx
@@ -1,6 +1,8 @@
-import { Tabs, Transition } from "@mantine/core";
+import { Box, Tabs, Transition } from "@mantine/core";
 import { BrandGithub, GitBranch } from "tabler-icons-react";
 import { BrandTwitter } from "tabler-icons-react";
+import { Photo, MessageCircle, Settings } from "tabler-icons-react";
+
 import { Anchor } from "@mantine/core";
 import Link from "next/link";
 
@@ -10,72 +12,27 @@ function Twitter() {
       size={30}
       strokeWidth={1}
       color={"black"}
-      className=" mx-2 "
+      className=" mx-3 pb-1 "
     />
   );
 }
 
 function Git() {
   return (
-    <BrandGithub size={30} strokeWidth={1} color={"black"} className=" mx-2 " />
+    <BrandGithub size={30} strokeWidth={1} color={"black"} className=" mx-3 pb-1 " />
   );
 }
 
 export const topMenu = () => {
   return (
-    <Tabs
-      tabPadding="md"
-      className="my-auto mb-2 flex justify-end px-10 pl-32"
-      classNames={{ tabLabel: "hover:text-red-300" }}
-      color="red"
-    >
-      <Link href="/">
-        <a className=" m-0 p-0 no-underline">
-          <Tabs.Tab
-            label="Home"
-            className=" font-Avenir text-lg text-black "
-          ></Tabs.Tab>
-        </a>
-      </Link>
-      <Link href="/postsList">
-        <a className="no-underline">
-          {" "}
-          <Tabs.Tab
-            label="Blog"
-            className=" font-Avenir text-lg text-black"
-          ></Tabs.Tab>
-        </a>
-      </Link>
-      <Link href="/myPortfolio">
-        <a className="no-underline">
-          <Tabs.Tab
-            label="MyPortfolio"
-            className=" font-Avenir text-lg text-black"
-          ></Tabs.Tab>
-        </a>
-      </Link>
-      <Link href="/contactPage">
-        <a className="no-underline">
-          <Tabs.Tab
-            label="Contact"
-            className=" font-Avenir text-lg text-black"
-          ></Tabs.Tab>
-        </a>
-      </Link>
-
-      <BrandGithub
-        className="mx-3 rounded-full hover:bg-red-50"
-        size={30}
-        strokeWidth={1}
-        color={"black"}
-      ></BrandGithub>
-
-      <BrandTwitter
-        className="mx-3 rounded-full hover:bg-red-50"
-        size={30}
-        strokeWidth={1}
-        color={"black"}
-      ></BrandTwitter>
-    </Tabs>
+    <Box className="flex items-end">
+      <Tabs color="red">
+        <Tabs.Tab label="HOME"></Tabs.Tab>
+        <Tabs.Tab label="BLOG"></Tabs.Tab>
+        <Tabs.Tab label="CONTACT"></Tabs.Tab>
+      </Tabs>
+      {Git()}
+      {Twitter()}
+    </Box>
   );
 };
